55043e2f9fade769f473953799bfb76966489dee
tedd2019 <tedd_hertel@unigroup.com>
2024-10-23 10:57:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 # Import python packages
-import streamlit as st
+import streamlit as st 
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
