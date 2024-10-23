60c50ea57b4564acf464c24a595dca82d03b8ac7
tedd2019 <tedd_hertel@unigroup.com>
2024-10-23 14:08:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from snowflake.snowpark.functions import col
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
 cnx = st.connection("snowflake")
