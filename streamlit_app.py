6dddd832eb24a1e3a8b09d3b05b8759bd1ac0b48
tedd2019 <tedd_hertel@unigroup.com>
2024-10-23 13:49:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-cnx = st.connection("Snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 # Write directly to the app
